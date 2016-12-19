e2ff8d59bc84581081cfb20ab71c16aea0aaf5f6
Jonas Moeller <jonas.moeller@wlw.de>
2016-12-19 23:25:11 +0100

    Page can be selected and is stored

--- gridlink-manager.php
+++ gridlink-manager.php
@@ -4,6 +4,19 @@ if(!current_user_can('manage_polls')) {
     die('Access Denied');
 }
 
+$page_select_args = $args = array(
+    'depth'                 => 2,
+    'child_of'              => 0,
+    'selected'              => 0,
+    'echo'                  => 1,
+    'name'                  => 'link',
+    'id'                    => 'page-link', // string
+    'class'                 => null, // string
+    'show_option_none'      => null, // string
+    'show_option_no_change' => null, // string
+    'option_none_value'     => null, // string
+);
+
 if(isset($_POST['title'])) {
   $wpdb->grid_link = $wpdb->prefix.'grid_link';
   $title = strip_tags($_POST['title'], '');
@@ -20,7 +33,10 @@ if(isset($_POST['title'])) {
 ?>
 <h1>Add a new Gridlink</h1>
 <form method='POST' action="<?php echo admin_url('admin.php?page='.plugin_basename(__FILE__)); ?>">
-  <input type='text' name='title' placeholder='Titel des Gridlinks'/>
-  <input type='text' name='link' placeholder='Seite auf die gelinkt wird'/>
+  <label for='grid-title'>Title: </label>
+  <input id='grid-title' type='text' name='title' placeholder='Titel des Gridlinks'/>
+  <br />
+  <label for='gridtitle'>Page Link:</label>
+  <?php wp_dropdown_pages( $args ); ?>
   <?php submit_button('Speichern') ?>
 </form>
